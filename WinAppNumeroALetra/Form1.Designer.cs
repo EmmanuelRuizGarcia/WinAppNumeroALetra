7565e5ac1d64b8d3b7c35a1bab48c0d50778bd92
Emmanuel Ruiz Garcia <emmanuel.ruiz.garcia@outlook.com>
2022-07-26 14:02:29 -0500

    se implemento la clase para poder convertir numeros a letras

--- WinAppNumeroALetra/Form1.Designer.cs
+++ WinAppNumeroALetra/Form1.Designer.cs
@@ -29,13 +29,59 @@ namespace WinAppNumeroALetra
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(38, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(255, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(309, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Convertir a Letra";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Cantidad en Letra";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(739, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- WinAppNumeroALetra/Form1.cs
+++ WinAppNumeroALetra/Form1.cs
@@ -16,5 +16,23 @@ namespace WinAppNumeroALetra
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Numalet let = new Numalet();
+            //al uso en México (creo):
+            let.MascaraSalidaDecimal = "00/100 M.N.";
+            let.SeparadorDecimalSalida = "pesos";
+            //observar que sin esta propiedad queda "veintiuno pesos" en vez de "veintiún pesos":
+            let.ApocoparUnoParteEntera = true;
+            var cantidadLetra = let.ToCustomCardinal(textBox1.Text);
+            label1.Text = cantidadLetra;
+            MessageBox.Show(cantidadLetra);
+        }
     }
 }
